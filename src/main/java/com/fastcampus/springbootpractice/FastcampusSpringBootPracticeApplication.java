ea72b214a3cb86f9c429d0573b640bb02c6341a1
Uno <djkehh@gmail.com>
2021-07-08 05:44:06 +0900

    Initialize #1 - spring boot
    스프링 부트 셋업 완료

---
+++ src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.springbootpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusSpringBootPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusSpringBootPracticeApplication.class, args);
+    }
+
+}
