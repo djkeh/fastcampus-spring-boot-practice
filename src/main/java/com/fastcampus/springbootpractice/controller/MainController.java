6b208ceadeb70e07b4132d2f6aee0d9f3d3686df
Uno <djkehh@gmail.com>
2021-07-08 06:01:34 +0900

    Copy #1 - source codes
    지난 스프링 예제에서 사용했던 코드를 그대로 복사해서 이식

---
+++ src/main/java/com/fastcampus/springbootpractice/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.fastcampus.springbootpractice.controller;
+
+import com.fastcampus.springbootpractice.service.SortService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MainController {
+
+    private final SortService sortService;
+
+    public MainController(SortService sortService) {
+        this.sortService = sortService;
+    }
+
+    @GetMapping("/")
+    public String hello(@RequestParam List<String> list) {
+        return sortService.doSort(list).toString();
+    }
+
+}
