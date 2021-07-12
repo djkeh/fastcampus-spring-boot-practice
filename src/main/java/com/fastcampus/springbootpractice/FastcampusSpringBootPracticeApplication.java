6c42955937875b84c67865fbf9cdc01c4f7c6edd
Uno <djkehh@gmail.com>
2021-07-13 05:26:49 +0900

    Experiment #3 - 프로파일러 테스트
    Flight Recorder, BufferingApplicationStartup 테스트
    
    * flight recorder
      * 코드 짠다
      * 프롬프트에서 `java -XX:StartFlightRecording:filename=recording.jfr,duration=10s -jar ${jar파일}` 로 부트 실행
        * 이 내용은 `FlightRecorderApplicationStartup` 안에 javadoc으로 가이드 되어 있음
      * 부트 실행 종료
      * 프로젝트 루트에서 `recording.jfr` 확인
      * JDK Mission Control 실행
      * 파일 로드
    * BufferingApplicationStartup
      * 코드 구현
      * 평범하게 부트 실행
      * actuator로 `/startup` 항목 추가된 것 확인
      * POST 로 데이터 조회: `curl -X POST http://localhost:8080/actuator/startup | jq`

--- src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
+++ src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
@@ -2,14 +2,19 @@ package com.fastcampus.springbootpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.core.metrics.jfr.FlightRecorderApplicationStartup;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
 public class FastcampusSpringBootPracticeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FastcampusSpringBootPracticeApplication.class, args);
+        SpringApplication application = new SpringApplication(FastcampusSpringBootPracticeApplication.class);
+//        application.setApplicationStartup(new FlightRecorderApplicationStartup());
+        application.setApplicationStartup(new BufferingApplicationStartup(1000));
+        application.run(args);
     }
 
 }
