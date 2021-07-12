ee1f20641a1de8233334760bd7ff2817bd82ae59
Uno <djkehh@gmail.com>
2021-07-13 05:20:59 +0900

    Experiment #3 - configuration properties in variety of methods
    여러 방법으로 properties 사용 방법을 실험
    행한 실험은:
    
    * actuator endpoint 전부 개방
    * `@ConstructorBinding` -> 2.2 기능
      * `@DefaultValue` -> 2.2 기능
      * `@DurationUnit`을 `@ConstructorBinding` 생성자에 사용
      * `@name`
    * `.properties` 에 yaml 처럼 문서 구분하기
      * 프로파일 설정
      * 부트 프로퍼티 변화: `spring.profiles` -> `spring.config.activate.on-profile`
    * `spring.config.import`
      * 여러개 `spring.config.import`
      * config tree
      * 확장자 없는 properties 파일

--- src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
+++ src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
@@ -2,7 +2,9 @@ package com.fastcampus.springbootpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class FastcampusSpringBootPracticeApplication {
 
