460595afd377da54b0c7467c9df625b38e34a068
Uno <djkehh@gmail.com>
2021-07-18 07:01:22 +0900

    Remove #5 - 연습코드 삭제
    연습한 내용을 삭제하고 최종 `@ConstructorBinding`만
    남김

--- src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
+++ src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
@@ -1,12 +1,9 @@
 package com.fastcampus.springbootpractice;
 
 import com.fastcampus.springbootpractice.properties.MyProperties;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.ApplicationContext;
-import org.springframework.core.env.Environment;
 
 import javax.annotation.PostConstruct;
 
@@ -14,20 +11,9 @@ import javax.annotation.PostConstruct;
 @SpringBootApplication
 public class FastcampusSpringBootPracticeApplication {
 
-    private final Integer height;
-    private final Environment environment;
-    private final ApplicationContext applicationContext;
     private final MyProperties myProperties;
 
-    public FastcampusSpringBootPracticeApplication(
-            @Value("${my.height}") Integer height,
-            Environment environment,
-            ApplicationContext applicationContext,
-            MyProperties myProperties
-    ) {
-        this.height = height;
-        this.environment = environment;
-        this.applicationContext = applicationContext;
+    public FastcampusSpringBootPracticeApplication(MyProperties myProperties) {
         this.myProperties = myProperties;
     }
 
@@ -37,11 +23,7 @@ public class FastcampusSpringBootPracticeApplication {
 
     @PostConstruct
     public void init() {
-        System.out.println("[@Value] " + height);
-        System.out.println("[Environment] " + environment.getProperty("my.height"));
-        System.out.println("[ApplicationContect] " + applicationContext.getEnvironment().getProperty("my.height"));
         System.out.println("[configurationProps] " + myProperties.getHeight());
-
     }
 
 }
