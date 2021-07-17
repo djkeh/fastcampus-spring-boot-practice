7f7b794ab573f012535377bb426bc7f3ec09f3ad
Uno <djkehh@gmail.com>
2021-07-18 06:59:08 +0900

    Practice #5 - 다양한 형태로 프로퍼티 읽기
    4가지 서로 다른 프로퍼티 읽기 방법을 연습
    
    * `@Value`
    * `Environment`
    * `ApplicationContext`
    * `@ConstructorBinding`

--- src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
+++ src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
@@ -1,13 +1,47 @@
 package com.fastcampus.springbootpractice;
 
+import com.fastcampus.springbootpractice.properties.MyProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 
+import javax.annotation.PostConstruct;
+
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class FastcampusSpringBootPracticeApplication {
 
+    private final Integer height;
+    private final Environment environment;
+    private final ApplicationContext applicationContext;
+    private final MyProperties myProperties;
+
+    public FastcampusSpringBootPracticeApplication(
+            @Value("${my.height}") Integer height,
+            Environment environment,
+            ApplicationContext applicationContext,
+            MyProperties myProperties
+    ) {
+        this.height = height;
+        this.environment = environment;
+        this.applicationContext = applicationContext;
+        this.myProperties = myProperties;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(FastcampusSpringBootPracticeApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("[@Value] " + height);
+        System.out.println("[Environment] " + environment.getProperty("my.height"));
+        System.out.println("[ApplicationContect] " + applicationContext.getEnvironment().getProperty("my.height"));
+        System.out.println("[configurationProps] " + myProperties.getHeight());
+
     }
 
 }
---
+++ src/main/java/com/fastcampus/springbootpractice/config/Config.java
@@ -0,0 +1,17 @@
+package com.fastcampus.springbootpractice.config;
+
+import com.fastcampus.springbootpractice.properties.MyProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+//    @ConfigurationProperties("my")
+//    @Bean
+//    public MyProperties myProperties() {
+//        return new MyProperties();
+//    }
+
+}
