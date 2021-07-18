251410427d1a7b512b516a1ef5c26a5c1577a45f
Uno <djkehh@gmail.com>
2021-07-19 02:39:12 +0900

    Add #11 - StudentService into main program
    학생 서비스가 부트에서 실행되어 결과를 보여주게끔 작성

--- src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
+++ src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
@@ -1,6 +1,7 @@
 package com.fastcampus.springbootpractice;
 
 import com.fastcampus.springbootpractice.properties.MyProperties;
+import com.fastcampus.springbootpractice.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,6 +15,7 @@ import javax.annotation.PostConstruct;
 public class FastcampusSpringBootPracticeApplication {
 
     private final MyProperties myProperties;
+    private final StudentService studentService;
 
     public static void main(String[] args) {
         SpringApplication.run(FastcampusSpringBootPracticeApplication.class, args);
@@ -22,6 +24,7 @@ public class FastcampusSpringBootPracticeApplication {
     @PostConstruct
     public void init() {
 //        System.out.println("내 키는: " + myProperties.getHeight());
+        studentService.printStudent("jack");
     }
 
 }
