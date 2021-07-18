21fb8dcb8448aa93fca9a342ab112dd87e1c7427
Uno <djkehh@gmail.com>
2021-07-19 07:03:34 +0900

    Update #11 - redis config
    설정하는 좀 더 간편하고 덜 장황한 방법

--- src/main/java/com/fastcampus/springbootpractice/config/RedisConfig.java
+++ src/main/java/com/fastcampus/springbootpractice/config/RedisConfig.java
@@ -1,28 +1,19 @@
 package com.fastcampus.springbootpractice.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager.RedisCacheManagerBuilder;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 
-@RequiredArgsConstructor
 @Configuration
 public class RedisConfig extends CachingConfigurerSupport {
 
-    private final RedisConnectionFactory redisConnectionFactory;
-
-    @Override
-    public CacheManager cacheManager() {
-        return RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory)
-                .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig()
-                        .serializeValuesWith(SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
-                )
-                .build();
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
     }
 
 }
