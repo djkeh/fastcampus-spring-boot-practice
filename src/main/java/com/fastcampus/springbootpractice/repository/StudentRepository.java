8b98d99513c1a634477328af1f9bfeb1ad1ccbde
Uno <djkehh@gmail.com>
2021-07-19 02:49:56 +0900

    Refactor #11 - unnecessary feature in repository
    학생 저장소 클래스에 `of()` 팩토리 메소드를 만들었는데
    왜 만들었지? 습관적으로 한 것 같음
    이러면 생성자가 private 이 되기 때문에
    프레임워크가 접근하는 상황에는 문제가 될 수 있음
    필요 없으므로 삭제

--- src/main/java/com/fastcampus/springbootpractice/repository/StudentRepository.java
+++ src/main/java/com/fastcampus/springbootpractice/repository/StudentRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Map;
 
-@RequiredArgsConstructor(staticName = "of")
+@RequiredArgsConstructor
 @Repository
 public class StudentRepository {
 
