4b1a51182636853176309fd6e3de309f8207b2ee
Uno <djkehh@gmail.com>
2021-07-19 07:02:07 +0900

    Add #11 - redis config
    레디스 데이터를 jackson 을 써서 json 으로 넣도록 설정 추가
    현재 이 기능은 부트 프로퍼티도 지원하지 않는 것 같음
    
    `Student` 는 더이상 자바 직렬화를 쓰지 않으므로
    불필요한 `Serializable` 제거
    
    jackson 이 읽으려면 기본 생성자가 필요하므로
    `@NoArgsConstructor` 추가

--- src/main/java/com/fastcampus/springbootpractice/domain/Student.java
+++ src/main/java/com/fastcampus/springbootpractice/domain/Student.java
@@ -2,12 +2,12 @@ package com.fastcampus.springbootpractice.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
-
+@NoArgsConstructor
 @AllArgsConstructor(staticName = "of")
 @Data
-public class Student implements Serializable {
+public class Student {
     private String name;
     private Integer age;
     private Grade grade;
