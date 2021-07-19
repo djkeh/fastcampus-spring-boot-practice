6930964ae2035b911978079b17ab717aad718bc5
Uno <djkehh@gmail.com>
2021-07-20 02:31:25 +0900

    Observe #13 - vault config result
    볼트를 적용해 값을 가져온 결과를 관찰
    편의상 `@Value`를 사용하는데,
    생성자 주입을 하려고 하면 롬복을 사용하기 어려워지는
    단점이 있음

--- src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
+++ src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
@@ -3,19 +3,33 @@ package com.fastcampus.springbootpractice;
 import com.fastcampus.springbootpractice.properties.MyProperties;
 import com.fastcampus.springbootpractice.service.StudentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.event.EventListener;
 
-@RequiredArgsConstructor
 @ConfigurationPropertiesScan
 @SpringBootApplication
 public class FastcampusSpringBootPracticeApplication {
 
     private final MyProperties myProperties;
     private final StudentService studentService;
+    private final String username;
+    private final String password;
+
+    public FastcampusSpringBootPracticeApplication(
+            MyProperties myProperties,
+            StudentService studentService,
+            @Value("${username}") String username,
+            @Value("${password}") String password
+    ) {
+        this.myProperties = myProperties;
+        this.studentService = studentService;
+        this.username = username;
+        this.password = password;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FastcampusSpringBootPracticeApplication.class, args);
@@ -30,6 +44,8 @@ public class FastcampusSpringBootPracticeApplication {
 //        studentService.printStudent("fred");
 //        studentService.printStudent("cassie");
 //        studentService.printStudent("cassie");
+        System.out.println("user: " + username);
+        System.out.println("pw: " + password);
     }
 
 }
