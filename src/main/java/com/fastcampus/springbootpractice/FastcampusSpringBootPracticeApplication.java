f1a869e3dcc32994ce5d2f497eab7f1e05ce5263
Uno <djkehh@gmail.com>
2021-07-21 02:36:07 +0900

    Disable #14 - vault
    네이티브 이미지 빌드 후 실행해보니 에러.
    vault 와 관련된 에러가 관찰됨
    vault 호환 안 되거나 vault 서버 접근에 이슈가 있는 듯?
    제외

--- src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
+++ src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
@@ -22,8 +22,8 @@ public class FastcampusSpringBootPracticeApplication {
     public FastcampusSpringBootPracticeApplication(
             MyProperties myProperties,
             StudentService studentService,
-            @Value("${spring.datasource.username:}") String username,
-            @Value("${spring.datasource.password:}") String password
+            @Value("${spring.datasource.username:vault가}") String username,
+            @Value("${spring.datasource.password:꺼져 있어요}") String password
     ) {
         this.myProperties = myProperties;
         this.studentService = studentService;
@@ -44,8 +44,8 @@ public class FastcampusSpringBootPracticeApplication {
 //        studentService.printStudent("fred");
 //        studentService.printStudent("cassie");
 //        studentService.printStudent("cassie");
-//        System.out.println("user: " + username);
-//        System.out.println("pw: " + password);
+        System.out.println("user: " + username);
+        System.out.println("pw: " + password);
     }
 
 }
