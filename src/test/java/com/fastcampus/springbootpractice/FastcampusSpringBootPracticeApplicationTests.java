10f042ba1e2b755c442c304c75319e5ce7b145c1
Uno <djkehh@gmail.com>
2021-07-22 23:22:37 +0900

    Apply #15 - redis testcontainers into test
    testcontainers 를 이용해 redis 를
    도커 이미지로 등록하는 방법

--- src/test/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplicationTests.java
+++ src/test/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplicationTests.java
@@ -2,12 +2,28 @@ package com.fastcampus.springbootpractice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
 @SpringBootTest
 class FastcampusSpringBootPracticeApplicationTests {
 
+    @Container
+    private static final GenericContainer<?> redisContainer = new GenericContainer<>(DockerImageName.parse("redis:latest"));
+
     @Test
     void contextLoads() {
+        // Given
+
+        // When
+
+        // Then
+        assertThat(redisContainer.isRunning()).isTrue();
     }
 
 }
