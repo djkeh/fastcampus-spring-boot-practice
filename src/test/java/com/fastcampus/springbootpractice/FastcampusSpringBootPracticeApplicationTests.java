a3e5656513d62db75ebf8fd298803db08f6477e1
Uno <djkehh@gmail.com>
2021-07-22 23:24:59 +0900

    Add #15 - logger inside testcontainer
    테스트 컨테이너에 로거를 붙여서
    이제 컨테이너 안에서 무슨 일이 일어나는지 관찰 가능

--- src/test/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplicationTests.java
+++ src/test/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplicationTests.java
@@ -1,10 +1,14 @@
 package com.fastcampus.springbootpractice;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
@@ -15,8 +19,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class FastcampusSpringBootPracticeApplicationTests {
 
+    private static final Logger logger = LoggerFactory.getLogger(FastcampusSpringBootPracticeApplicationTests.class);
+
     @Container
     private static final GenericContainer<?> redisContainer = new GenericContainer<>(DockerImageName.parse("redis:latest"));
+
+    @BeforeAll
+    static void setup() {
+        redisContainer.followOutput(new Slf4jLogConsumer(logger));
+    }
 
     @DynamicPropertySource
     static void properties(DynamicPropertyRegistry registry) {
