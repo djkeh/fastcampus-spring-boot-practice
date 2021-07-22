0df4b3515e0b8b7442abc0ff5556a23e20bf3021
Uno <djkehh@gmail.com>
2021-07-22 23:28:03 +0900

    Test #15 - redis cache using testcontainers
    의미 있는 테스트 코드의 작성
    
    * 컨테이너가 실행되었는가? - ok
    * 레디스 키가 의도한 대로 저장되었는가? - no
    * 레디스 값이 의도한 대로 저장되었는가? - no
    
    기존 Jackson serializer 를 사용하지 않았기 때문에
    `Student` 에 또 `Serializable` 을 붙임

--- src/main/java/com/fastcampus/springbootpractice/domain/Student.java
+++ src/main/java/com/fastcampus/springbootpractice/domain/Student.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @NoArgsConstructor
 @AllArgsConstructor(staticName = "of")
 @Data
-public class Student {
+public class Student implements Serializable {
     private String name;
     private Integer age;
     private Grade grade;
--- src/test/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplicationTests.java
+++ src/test/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplicationTests.java
@@ -35,10 +35,16 @@ class FastcampusSpringBootPracticeApplicationTests {
     }
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
         // Given
 
         // When
+        GenericContainer.ExecResult execResult1 = redisContainer.execInContainer("redis-cli", "get", "student::cassie");
+        GenericContainer.ExecResult execResult2 = redisContainer.execInContainer("redis-cli", "get", "student::fred");
+        GenericContainer.ExecResult execResult3 = redisContainer.execInContainer("redis-cli", "get", "student::jack");
+        System.out.println(execResult1.getStdout());
+        System.out.println(execResult2.getStdout());
+        System.out.println(execResult3.getStdout());
 
         // Then
         assertThat(redisContainer.isRunning()).isTrue();
