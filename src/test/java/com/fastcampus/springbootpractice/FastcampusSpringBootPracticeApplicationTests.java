8244508f558678240c12ec21f857e6eb72cc3a06
Uno <djkehh@gmail.com>
2021-07-22 23:24:02 +0900

    Correct #15 - "Unable to connect to Redis" - @DynamicPropertySource
    테스트 컨테이너를 사용했는데 redis 에 연결 못 하는 이유는
    redis 컨테이너가 매번 랜덤 포트를 사용하기 때문
    
    포트 번호를 미리 알 수 없으므로
    `application.properties` 에서 세팅할 수 없기 떄문에
    테스트 도중, 컨테이너를 올린 직후 포트 번호를 알아내서
    프로퍼티 주입하는 dynamic 한 방법이 필요
    
    Spring Framework 5.2.5 에서 처음 도입된 이 기능으로
    문제를 해결

--- src/test/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplicationTests.java
+++ src/test/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplicationTests.java
@@ -2,6 +2,8 @@ package com.fastcampus.springbootpractice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -15,6 +17,11 @@ class FastcampusSpringBootPracticeApplicationTests {
 
     @Container
     private static final GenericContainer<?> redisContainer = new GenericContainer<>(DockerImageName.parse("redis:latest"));
+
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.redis.port", () -> redisContainer.getMappedPort(6379));
+    }
 
     @Test
     void contextLoads() {
